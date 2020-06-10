2913288ce8b9e5302e6185f58f068a2838f0f820
liuyuling <944898701@qq.com>
2020-06-10 22:09:59 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: '搬砖的艺术',
+  description: '个人技术分享、记录。',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  markdown: {
+    lineNumbers: true,
+  }
+}
